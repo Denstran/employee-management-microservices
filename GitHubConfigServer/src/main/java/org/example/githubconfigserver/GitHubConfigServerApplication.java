e7d0f6936137c2fde9898eaf056788deb46849ce
Kirill Stradze <akamanapa@gmail.com>
2024-07-16 15:30:42 +0300

    Create GitHubConfigServer

---
+++ GitHubConfigServer/src/main/java/org/example/githubconfigserver/GitHubConfigServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubConfigServerApplication.class, args);
+    }
+
+}

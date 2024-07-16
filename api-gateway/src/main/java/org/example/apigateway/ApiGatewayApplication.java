e317931db820cf792b5527a75833358af77279e9
Kirill Stradze <akamanapa@gmail.com>
2024-07-16 16:09:50 +0300

    Create api-gateway, eureka-server

--- api-gateway/src/main/java/org/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/example/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,13 @@
-package org.example.githubconfigserver;
+package org.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GitHubConfigServerApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GitHubConfigServerApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
